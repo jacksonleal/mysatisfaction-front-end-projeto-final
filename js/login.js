8e63f981d8fa1d4e5f37eccd80ebdf5ae195f83f
jackson <lealjackson@hotmail.com>
2019-12-19 10:55:00 -0300

    leal-jack-links-das-telas

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 let btnLogin = document.querySelector("#login");
 let btnInscreverse = document.querySelector("#inscreverse");
 let body = document.querySelector("body");
+let btnLogarNoSis = document.querySelector("#logarNoSis");
 
 btnLogin.addEventListener("click", function () {
   body.className = "fazer-login-js";
@@ -8,4 +9,9 @@ btnLogin.addEventListener("click", function () {
 
 btnInscreverse.addEventListener("click", function () {
   body.className = "cadastrar-se-js";
+});
+
+btnLogarNoSis.addEventListener("click", function () {
+  event.preventDefault();
+  window.location = "CriadorDeForms.html";
 });
