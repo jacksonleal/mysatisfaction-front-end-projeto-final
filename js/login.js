21764ac87ad64194deda881c7017cf7894458fd5
jackson <lealjackson@hotmail.com>
2020-01-27 08:29:23 -0300

    test get user

--- js/login.js
+++ js/login.js
@@ -1,17 +1,107 @@
 let btnLogin = document.querySelector("#login");
 let btnInscreverse = document.querySelector("#inscreverse");
 let body = document.querySelector("body");
-let btnLogarNoSis = document.querySelector("#logarNoSis");
+
+function inscreverse() {
+  body.className = "cadastrar-se-js";
+};
+
+function idLogin() {
+  body.className = "cadastrar-se-js";
+}
 
 btnLogin.addEventListener("click", function () {
   body.className = "fazer-login-js";
 });
 
-btnInscreverse.addEventListener("click", function () {
-  body.className = "cadastrar-se-js";
-});
-
-btnLogarNoSis.addEventListener("click", function () {
+//*******************************************/ criar usuário
+function MyFucLogin() {
   event.preventDefault();
-  window.location = "CriadorDeForms.html";
-});
+
+  var userEmail = document.getElementById("emailLogin").value;
+  var userPass = document.getElementById("passLogin").value;
+
+  var url = "https://mysatisfaction-project-back-en.herokuapp.com/users/login/" + userEmail + "/" + userPass;
+
+  // create an XHR object
+  const xhr = new XMLHttpRequest();
+
+  // listen for `load` event
+  xhr.onload = () => {
+    if (xhr.response != "[]") {
+      window.location = "CriadorDeForms.html?" + userEmail;
+
+    } else {
+      alert("E-mail ou senha incorretos");
+    }
+  };
+
+  // listen for `error` event
+  xhr.onerror = () => {
+    console.error('Request failed.');
+  }
+
+  // create a `GET` request
+  xhr.open('GET', url, true);
+
+  // send request
+  xhr.send();
+
+  //fim login fun
+}
+
+function inscClick() {
+  event.preventDefault();
+
+  var usernAME = document.getElementById("username").value;
+  var userEmail = document.getElementById("useremail").value;
+  var userPass = document.getElementById("userpassword").value;
+  var userRole = "admin";
+
+  var url = "https://mysatisfaction-project-back-en.herokuapp.com/users/login/";
+  // Get a user
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', url + userEmail, true)
+  xhr.onload = function () {
+
+    if (xhr.response != "[]") {
+      alert('Este E-mail já está cadastrado em nosso sistema!')
+      return;
+    } else {
+      cadastradaUser(usernAME, userEmail, userPass, userRole);
+    }
+  }
+  xhr.send(null);
+
+  //
+}
+
+function cadastradaUser(usernAME, userEmail, userPass, userRole) {
+  //test
+
+  var url = "https://mysatisfaction-project-back-en.herokuapp.com/users";
+
+  // Post usuário
+  var data = {};
+  data.name = usernAME;
+  data.email = userEmail;
+  data.password = userPass;
+  data.role = userRole;
+  var json = JSON.stringify(data);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  xhr.onload = function () {
+    var users = JSON.parse(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "201") {
+      alert("Usúario cadastrado com sucesso!");
+      body.className = "fazer-login-js";
+    } else {
+      alert('Não foi possivel cadastrar o usuário');
+    }
+  }
+
+  xhr.send(json);
+
+}
--- js/main.js
+++ js/main.js
@@ -1,9 +1,61 @@
-$(document).on('click', '._paisagem', function () {
-  $('.table').removeClass('retrato').addClass('paisagem');
-});
-$(document).on('click', '._retrato', function () {
-  $('.table').removeClass('paisagem').addClass('retrato');
-});
+
+window.onload = function () {
+  var url_atual = window.location.href;
+  url_atual = url_atual.replace('http://mysatisfaction.surge.sh/CriadorDeForms.html?', '');
+
+  var url = "https://mysatisfaction-project-back-en.herokuapp.com/users/email/" + url_atual;
+
+  // create an XHR object
+  const xhr = new XMLHttpRequest();
+
+  // listen for `load` event
+  xhr.onload = () => {
+    if (xhr.response != "[]") {
+      sbvnc.innerHTML = "Seja bem-vindo(a), " + xhr.response;
+
+    } else {
+      alert("Aogo deu errado.");
+    }
+  };
+
+  // listen for `error` event
+  xhr.onerror = () => {
+    console.error('Request failed.');
+  }
+
+  // create a `GET` request
+  xhr.open('GET', url, true);
+
+  // send request
+  xhr.send();
+
+};
+
+function myFuncSalvar() {
+  event.preventDefault();
+
+  //
+
+  var user = document.getElementById("texarea2").value + "/" +
+    document.getElementById("texarea1").value;
+
+
+  var xmlHttp = "https://mysatisfaction-project-back-en.herokuapp.com/cforms/" + user;
+
+  //requisição POST
+  var ajax = new XMLHttpRequest();
+
+  // Seta tipo de requisição: Post e a URL da API
+  ajax.open("POST", xmlHttp, true);
+  ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  // Seta paramêtros da requisição e envia a requisição
+  ajax.send();
+
+  document.getElementById("texarea2").value = "";
+  document.getElementById("texarea1").value = "";
+
+}
 
 function mostrarAtivo(tag) {
   let tag_li = document.getElementById('lista_menu');
@@ -21,4 +73,6 @@ function keydownFuncao() {
   document.getElementById("h1KeyDown").innerHTML = texto;
 }
 
+
+//
 
