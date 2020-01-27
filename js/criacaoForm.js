3b866ef4247d855594b4845382033997a859b91a
jackson <lealjackson@hotmail.com>
2020-01-27 17:15:32 -0300

    fazendo testes e arrumando td

--- js/criacaoForm.js
+++ js/criacaoForm.js
@@ -1,6 +1,44 @@
 let btnLogin = document.querySelector("#lbl-avaliar");
 let body = document.querySelector("body");
 
+window.onload = function () {
+  var url_atual = window.location.href;
+  //no Surge
+  url_atual = url_atual.replace('http://mysatisfaction.surge.sh/formDeAvaliacao.html?', '');
+  //localhost
+  //url_atual = url_atual.replace('file:///C:/Users/pc/Documents/LEAL/ADS_IV/PROGIN_JESIEL/PROJETO%20FINAL/mysatisfaction-front-end-projeto-final/CriadorDeForms.html?', '');
+
+  var url = "https://mysatisfaction-project-back-en.herokuapp.com/cforms/title/" + url_atual;
+
+  // create an XHR object
+  const xhr = new XMLHttpRequest();
+
+  // listen for `load` event
+  xhr.onload = () => {
+    if (xhr.response != "[]") {
+      tapc.innerHTML = xhr.response;
+
+    } else {
+      alert("Aogo deu errado.");
+    }
+  };
+
+  // listen for `error` event
+  xhr.onerror = () => {
+    console.error('Request failed.');
+  }
+
+  // create a `GET` request
+  xhr.open('GET', url, true);
+
+  // send request
+  xhr.send();
+
+};
+
+
+
+
 btnLogin.addEventListener("click", function () {
   body.className = "m-obg-js";
 
@@ -9,3 +47,5 @@ btnLogin.addEventListener("click", function () {
   }, 5100);
 
 });
+
+
--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 
 window.onload = function () {
   var url_atual = window.location.href;
+  //no Surge
   url_atual = url_atual.replace('http://mysatisfaction.surge.sh/CriadorDeForms.html?', '');
+  //localhost
+  //url_atual = url_atual.replace('file:///C:/Users/pc/Documents/LEAL/ADS_IV/PROGIN_JESIEL/PROJETO%20FINAL/mysatisfaction-front-end-projeto-final/CriadorDeForms.html?', '');
 
   var url = "https://mysatisfaction-project-back-en.herokuapp.com/users/email/" + url_atual;
 
@@ -36,21 +39,33 @@ function myFuncSalvar() {
 
   //
 
-  var user = document.getElementById("texarea2").value + "/" +
-    document.getElementById("texarea1").value;
+  var title = document.getElementById("texarea2").value;
+  var description = document.getElementById("texarea1").value;
 
 
-  var xmlHttp = "https://mysatisfaction-project-back-en.herokuapp.com/cforms/" + user;
+  var url = "https://mysatisfaction-project-back-en.herokuapp.com/cforms";
 
-  //requisição POST
-  var ajax = new XMLHttpRequest();
+  var data = {};
+  data.title = title;
+  data.description = description;
 
-  // Seta tipo de requisição: Post e a URL da API
-  ajax.open("POST", xmlHttp, true);
-  ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  var json = JSON.stringify(data);
 
-  // Seta paramêtros da requisição e envia a requisição
-  ajax.send();
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  xhr.onload = function () {
+    var users = JSON.parse(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "201") {
+      alert("Modelo Salvo");
+
+    } else {
+      alert('Não foi possivel cadastrar o modelo');
+    }
+  }
+
+  xhr.send(json);
 
   document.getElementById("texarea2").value = "";
   document.getElementById("texarea1").value = "";
@@ -66,13 +81,64 @@ function mostrarAtivo(tag) {
   }
   tag.style.backgroundColor = "purple"; // altera o fundo
   tag.style.color = "#ffffff"; // altera a cor
+
+  if (tag.id == "mf") {
+    getCforms();
+  }
+
+}
+
+function getCforms() {
+
+  var url = "https://mysatisfaction-project-back-en.herokuapp.com/cforms";
+
+  // create an XHR object
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'text';
+  // listen for `load` event
+  xhr.onload = () => {
+    if (xhr.response != "[]") {
+
+      var jsonResponse = JSON.parse(xhr.responseText);
+      //btnPubliqr.innerHTML = jsonResponse[0].title;
+
+      for (var i = 0; i < jsonResponse.length; i++) {
+        // Buscar elemento pai
+        var elemento_pai = document.getElementById("btnPubliqrID");
+        // 1. Create the button
+        var button = document.createElement("button");
+        button.className = "btn btn-2";
+        button.innerHTML = jsonResponse[i].title;
+
+        // 2. Append somewhere
+        var body = elemento_pai;
+        body.appendChild(button);
+
+        // 3. Add event handler
+        button.addEventListener("click", function () {
+          window.location = "formDeAvaliacao.html?" + this.innerHTML;
+        });
+      }
+
+    } else {
+      alert("Houve um erro");
+    }
+  };
+
+  // listen for `error` event
+  xhr.onerror = () => {
+    console.error('Request failed.');
+  }
+
+  // create a `GET` request
+  xhr.open('GET', url, true);
+
+  // send request
+  xhr.send();
+
 }
 
 function keydownFuncao() {
   let texto = document.getElementById("texarea1").value;
   document.getElementById("h1KeyDown").innerHTML = texto;
 }
-
-
-//
-
